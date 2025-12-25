13820f370cde647a083968d2e7a5f89d19d47ffc
Kulkant <kulkantsharma9883@gmail.com>
2025-12-25 16:22:18 +0530

    added add route of get my orders

--- ecommerce-backend/src/routes/order.routes.js
+++ ecommerce-backend/src/routes/order.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   addOrderItems,
+  getMyOrders,
   getOrderById,
   updateOrderToPaid,
 } from "../controllers/order.controllers.js";
@@ -9,6 +10,7 @@ import protect from "../middlewares/auth.middlewares.js";
 const router = express.Router();
 
 router.post("/", protect, addOrderItems);
+router.get("/myorders", protect, getMyOrders);
 router.get("/:id", protect, getOrderById);
 router.put("/:id/pay", protect, updateOrderToPaid);
 
